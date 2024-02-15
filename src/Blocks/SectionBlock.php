023d9b27b678b2eb078f7a058508697e72719d3d
Alexandre Rouze <alexandre@petitbambou.com>
2024-02-16 00:18:59 +0100

    Feature (Section fields) : Add fields & block_id in Section block.

--- src/Blocks/SectionBlock.php
+++ src/Blocks/SectionBlock.php
@@ -3,6 +3,7 @@
 namespace Arouze\SlackMessageBuilder\Blocks;
 
 use Arouze\SlackMessageBuilder\Accessories\AccessoryInterface;
+use Arouze\SlackMessageBuilder\Exceptions\MissingFieldException;
 use Arouze\SlackMessageBuilder\Objects\TextObject;
 
 class SectionBlock implements BlockInterface
@@ -10,26 +11,67 @@ class SectionBlock implements BlockInterface
     private const SECTION_TYPE = 'section';
     // @doc https://api.slack.com/reference/block-kit/blocks#section
 
-    private TextObject $textObject;
+    private ?TextObject $textObject = null;
 
     private ?AccessoryInterface $accessory = null;
+
+    private ?string $blockId = null;
+
+    private array $block = [
+        'type' => self::SECTION_TYPE
+    ];
+    private array $fields = [];
     public function toArray(): array
     {
-        $block = [
-            'type' => self::SECTION_TYPE,
-            'text' => $this->textObject->toArray()
-        ];
+        $this->validate();
 
-        if (!is_null($this->accessory)) {
-            $block = array_merge(
-                $block,
-                [
-                    'accessory' => $this->accessory->toArray()
-                ]
-            );
+        $this
+            ->handleTextObject()
+            ->handleBlockId()
+            ->handleAccessory()
+            ->handleFields();
+
+        return $this->block;
+    }
+
+    private function handleTextObject(): self
+    {
+        if (!is_null($this->textObject)) {
+            $this->block['text'] = $this->textObject->toArray();
         }
 
-        return $block;
+        return $this;
+    }
+    private function handleBlockId(): self
+    {
+        if (!is_null($this->blockId)) {
+            $this->block['block_id'] = $this->blockId;
+        }
+
+        return $this;
+    }
+    private function handleAccessory(): self
+    {
+        if (!is_null($this->accessory)) {
+            $this->block['accessory'] = $this->accessory->toArray();
+        }
+
+        return $this;
+    }
+    private function handleFields(): self
+    {
+        if (count($this->fields)) {
+            $this->block['fields'] = $this->fields;
+        }
+
+        return $this;
+    }
+
+    private function validate(): void
+    {
+        if (is_null($this->textObject) && count($this->fields) === 0) {
+            throw new MissingFieldException(self::class, 'text, fields');
+        }
     }
 
     public function setTextObject(TextObject $textObject): self
@@ -43,6 +85,19 @@ class SectionBlock implements BlockInterface
     {
         $this->accessory = $accessory;
 
+        return $this;
+    }
+
+    public function addFields(TextObject $textObject): SectionBlock
+    {
+        $this->fields[] = $textObject->toArray();
+
+        return $this;
+    }
+
+    public function setBlockId(?string $blockId): SectionBlock
+    {
+        $this->blockId = $blockId;
         return $this;
     }
 }
--- tests/Blocks/SectionBlockTest.php
+++ tests/Blocks/SectionBlockTest.php
@@ -3,6 +3,8 @@
 namespace Arouze\Tests\Blocks;
 
 use Arouze\SlackMessageBuilder\Blocks\SectionBlock;
+use Arouze\SlackMessageBuilder\Exceptions\MissingFieldException;
+use Arouze\SlackMessageBuilder\Exceptions\TooMuchElementsException;
 use Arouze\Tests\AbstractSlackMessageBuilderBaseTestCase;
 use PHPUnit\Framework\Attributes\Group;
 
@@ -30,5 +32,60 @@ class SectionBlockTest extends AbstractSlackMessageBuilderBaseTestCase
                 ->setAccessory(self::buildImageAccessory())
                 ->toArray()
         );
+    }
+
+    public function testSectionBlockWithFields(): void
+    {
+        self::assertEquals(
+            [
+                'type' => 'section',
+                'fields' => [
+                    [
+                        'type' => 'plain_text',
+                        'text' => 'Banana'
+                    ],
+                    [
+                        'type' => 'plain_text',
+                        'text' => 'Apple'
+                    ],
+                    [
+                        'type' => 'plain_text',
+                        'text' => 'Kiwi'
+                    ]
+                ]
+            ],
+            (new SectionBlock())
+                ->addFields(self::buildTextObject()->setText('Banana'))
+                ->addFields(self::buildTextObject()->setText('Apple'))
+                ->addFields(self::buildTextObject()->setText('Kiwi'))
+                ->toArray()
+        );
+    }
+
+    public function testSectionBlockWithBlockId(): void
+    {
+        self::assertEquals(
+            [
+                'type' => 'section',
+                'text' => [
+                    'type' => 'plain_text',
+                    'text' => 'Simple text.'
+                ],
+                'block_id' => 'block_id_1'
+            ],
+            (new SectionBlock())
+                ->setTextObject(self::buildTextObject())
+                ->setBlockId('block_id_1')
+                ->toArray()
+        );
+    }
+
+    public function testSectionBlockWithoutFieldsAndText(): void
+    {
+        self::expectException(MissingFieldException::class);
+
+        (new SectionBlock())
+            ->setBlockId('block_id_1')
+            ->toArray();
     }
 }
